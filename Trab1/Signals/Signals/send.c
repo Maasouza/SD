d2d89d487ae0d0ea1ad37b2cf11b037f7972e946
Leonardo <leonardo@mail.example.com>
2016-04-21 12:32:41 -0300

    Error treatment in send.c

--- Trab1/Signals/Signals/send.c
+++ Trab1/Signals/Signals/send.c
@@ -1,33 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <signal.h>
-#include <errno.h>
-#include <unistd.h>
-#include <sys/types.h>
-
-#define SUCCESS 0
-
+#include <limits.h>
 
 int main(int argc, char * argv[])
 {
-	
-	printf("pid = %d\n", getpid());
-	//if (proc_id == )
 
 	if (argc < 3)
 	{
-		printf("Parameter Missing - Expecting PID and SIGNAL values\n");
+		printf("ERROR - Parameter Missing. Expecting PID and SIGNAL values\n");
+		exit(EXIT_FAILURE);
 	}
-	else
+
+	unsigned long proc_id = strtoul(argv[1], NULL, 10);
+	unsigned long sig_value = strtoul(argv[2], NULL, 10);
+
+	if (proc_id == 0 && !strcmp(argv[1], "0"))
 	{
-		long proc_id = strtol(argv[1], NULL, 10);
-		long sig_value = strtol(argv[2], NULL, 10);
-
-		if (kill(proc_id, sig_value) != 0)
-		{
-			printf("ERROR - Process with PID = %s does not exist\n", argv[1]);
-		}
+		perror("ERROR - Invalid PID value");
+		exit(EXIT_FAILURE);
 	}
 
+	if (sig_value == 0 && strcmp(argv[2], "0") != 0)
+	{
+		perror("ERROR - Invalid SIGNAL value");
+		exit(EXIT_FAILURE);
+	}
+
+	if (proc_id == ULONG_MAX || sig_value == ULONG_MAX)
+	{
+		perror("ERROR - Invalid parameter");
+		exit(EXIT_FAILURE);
+	}
+
+	if (kill(proc_id, sig_value) != 0)
+	{
+		printf("ERROR - Process with PID = %s does not exist\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 	return 0;
 }
