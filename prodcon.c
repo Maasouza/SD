172a84de8eb8125129f4aa961094ab43ec806dc8
Marcos <maasouza@poli.ufrj.br>
2016-05-14 00:45:35 -0300

    Add files via upload

---
+++ prodcon.c
@@ -0,0 +1,57 @@
+#include "prodcon.h"
+
+
+
+int main( int argc, const char* argv[] ){
+	int nProd,nCons;
+	/*----------------------|
+	| arg[1] # of producer  |
+	| arg[2] # of consumer  |
+	-----------------------*/
+	srand(time(NULL));
+
+	if(argc == 3){
+
+		nProd = atoi(argv[1]);
+		nCons = atoi(argv[2]);
+	
+	}else{
+	
+		nProd = NP;
+		nCons = NC;
+		printf("Missing parameters! Using default values\n");
+	
+	}
+
+	for(i = 0; i < mSize ; i++){
+		sMemory[i]=0;
+	}
+
+	pthread_t p_Threads[nProd];
+	pthread_t c_Threads[nProd];
+	
+	sem_init(&full,0,mSize);
+	sem_init(&empty,0,0);
+	pthread_mutex_init(&mutex_memory,NULL);
+
+	for(i = 0;i<nProd;i++){
+		pthread_create(&p_Threads[i],NULL,prod,NULL);
+	}
+
+	for(i = 0; i < nCons ; i++){
+		pthread_create(&c_Threads[i],NULL,cons,NULL);
+	}
+
+	for(i = 0;i < nProd ; i++){
+		pthread_join(p_Threads[i],NULL);
+	}
+
+	for(i = 0; i < nCons ; i++){
+		pthread_join(c_Threads[i],NULL);
+	}
+
+	sem_destroy(&full);
+	sem_destroy(&empty);
+	pthread_mutex_destroy(&mutex_memory);
+
+}
