f8d0fc6fdf77d399d2ba1250ef0b32eb0363ce01
maasouza <maasouza@poli.ufrj.br>
2016-07-12 22:25:12 -0300

    Update parte 1

--- trab3/1/client.py
+++ trab3/1/client.py
@@ -1,15 +1,73 @@
 import xmlrpclib
+import random
+import threading
 
-conn = xmlrpclib.ServerProxy("http://localhost:3000")
 
-l=[1,2,3,4,5]
-print("l = "),
-print(l)
+vecSize = 10**8
 
-l_2 = conn.pow(2,l)
-print("l^2 = "),
-print(l_2)
+#Recebendo o numero de threads
+nThreads = input("Number of Threads ")
 
-avg = conn.avg(l)
-print("avg = "),
-print(avg)
+#escolhendo a operção a ser feita
+operation = input("0 - Media do vetor\n1 - Soma do vetor\n2 - Vetor ao quadrado\n")
+
+#vetor de numeros aleatorios
+numbers = [] 
+
+#constante que determina o tamanho do array de cada thread
+k = vecSize/nThreads
+
+#Variavel que contem o retorno
+response=[0 for i in range(nThreads)]
+
+
+print("Populando array...")
+for i in xrange(vecSize):	
+	numbers.append(random.randint(1, 127));
+print("Array populado.")
+
+
+#Classe que implementa a thread
+class RPC_thread (threading.Thread):    
+    def __init__(self, threadID, name, op):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.function = op
+
+    def run(self):
+    	self.conn = xmlrpclib.ServerProxy("http://localhost:3000")
+        if(self.function==0):
+        	response[self.threadID] = self.conn.avg(numbers[self.threadID*k:(self.threadID+1)*k])	
+        elif(self.function==1):
+        	response[self.threadID] = self.conn.sum(numbers[self.threadID*k:(self.threadID+1)*k])
+        elif(self.function==2):
+        	response[self.threadID] = self.conn.pow(2,numbers[self.threadID*k:(self.threadID+1)*k])
+        else:
+        	print("Invalid function")
+
+threads = []
+
+print("Criando as threads")
+for i in range(nThreads):
+	thread =  RPC_thread(i,"Thread"+str(i),operation)
+	thread.start()
+	threads.append(thread)
+print("Threads criadas e iniciadas.")
+
+#aguardando as threads terminarem de executar
+for t in threads:
+	t.join()
+
+
+conn = xmlrpclib.ServerProxy("http://localhost")
+if(operation==0):
+	print("Avg : ")
+	print(conn.avg(response))
+elif(operation==1):
+	print("Sum : ")
+	print(conn.sum(response))
+elif(operation==2):
+	print("Vector ^ 2 : ")
+	print(response)
+
--- trab3/1/server.py
+++ trab3/1/server.py
@@ -1,5 +1,12 @@
-from SimpleXMLRPCServer import SimpleXMLRPCServer
+
 import xmlrpclib
+import SocketServer
+from SimpleXMLRPCServer import SimpleXMLRPCServer,SimpleXMLRPCRequestHandler
+ 
+#ThreadingMixIn - permite que um endereço permita mais de uma conexão.
+#Ele alterar o handler do objeto passado como parametro junto dele.
+class MultiXMLRPCServer(SocketServer.ThreadingMixIn,SimpleXMLRPCServer):
+	pass
 
 def vec_avg(l):
 	return sum(l)/len(l)
@@ -7,11 +14,17 @@ def vec_avg(l):
 def vec_pow(a,l):
 	return [pow(x,a) for x in l]
 
+def vec_sum(l):
+	return sum(l)
 
-server = SimpleXMLRPCServer(("localhost",3000))
+#Como queremos que ele forneça um servidor RPC multi-acessado passamos como parametro o handler do RPC
+server =  MultiXMLRPCServer(('localhost', 3000), SimpleXMLRPCRequestHandler)
 
+#registrando as funções no servidor
 server.register_function(vec_avg,"avg")
 server.register_function(vec_pow,"pow")
+server.register_function(vec_sum,"sum")
 
 print("Ready to serve")
+#iniciando o servidor RPC
 server.serve_forever()
